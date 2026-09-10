f9fd339eb0824ea37d2b9ce9a1938793e888ad7f
Kim-Tsok <appkim02@gmail.com>
2026-09-10 02:32:33 +0100

    changed middleware to proxy and fixed ambigious auth

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { nextCookies } from "better-auth/next-js";
 import { auth } from "./lib/auth";
 
-export default function middleware() {
+export default function proxy() {
   // This is a placeholder - need to fix the middleware properly
 }
