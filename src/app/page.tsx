b1e0f72c0caa98b4e0bd57bc24e1aeaa681ca958
Kim-Tsok <appkim02@gmail.com>
2026-09-14 16:16:26 +0100

    Fixed build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,6 @@ import { Features } from '../components/landing-page/Features';
 import type { Metadata } from 'next';
 import { APP_NAME } from '@/lib/constants/site';
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-export const instant = false;
-
 export const metadata: Metadata = {
   title: `${APP_NAME}`,
 };
